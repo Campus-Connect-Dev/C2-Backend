8d700904da55d0548a933981ef2408353435083f
suraj <surajrace21@gmail.com>
2025-07-19 20:35:51 +0530

    feat: firebase env deployed

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -148,22 +148,20 @@ if (isFirebaseFunction) {
     NODE_ENV: process.env['NODE_ENV'] || 'development',
     PORT: parseInt(process.env['PORT'] || '3001', 10),
     API_VERSION: process.env['API_VERSION'] || 'v1',
-    
-    FIREBASE_PROJECT_ID: process.env['FIREBASE_PROJECT_ID'] || '',
-    FIREBASE_PRIVATE_KEY: (process.env['FIREBASE_PRIVATE_KEY'] || '').replace(/\\n/g, '\n'),
-    FIREBASE_CLIENT_EMAIL: process.env['FIREBASE_CLIENT_EMAIL'] || '',
-    
-    FIREBASE_API_KEY: process.env['FIREBASE_API_KEY'] || '',
-    FIREBASE_AUTH_DOMAIN: process.env['FIREBASE_AUTH_DOMAIN'] || '',
-    FIREBASE_STORAGE_BUCKET: process.env['FIREBASE_STORAGE_BUCKET'] || '',
-    FIREBASE_MESSAGING_SENDER_ID: process.env['FIREBASE_MESSAGING_SENDER_ID'] || '',
-    FIREBASE_APP_ID: process.env['FIREBASE_APP_ID'] || '',
-    FIREBASE_MEASUREMENT_ID: process.env['FIREBASE_MEASUREMENT_ID'] || '',
+    FIREBASE_PROJECT_ID: process.env['FB_PROJECT_ID'] || '',
+    FIREBASE_PRIVATE_KEY: (process.env['FB_PRIVATE_KEY'] || '').replace(/\\n/g, '\n'),
+    FIREBASE_CLIENT_EMAIL: process.env['FB_CLIENT_EMAIL'] || '',
+    FIREBASE_API_KEY: process.env['FB_API_KEY'] || '',
+    FIREBASE_AUTH_DOMAIN: process.env['FB_AUTH_DOMAIN'] || '',
+    FIREBASE_STORAGE_BUCKET: process.env['FB_STORAGE_BUCKET'] || '',
+    FIREBASE_MESSAGING_SENDER_ID: process.env['FB_MESSAGING_SENDER_ID'] || '',
+    FIREBASE_APP_ID: process.env['FB_APP_ID'] || '',
+    FIREBASE_MEASUREMENT_ID: process.env['FB_MEASUREMENT_ID'] || '',
     
     fb: {
-      project_id: process.env['FIREBASE_PROJECT_ID'] || '',
-      private_key: (process.env['FIREBASE_PRIVATE_KEY'] || '').replace(/\\n/g, '\n'),
-      client_email: process.env['FIREBASE_CLIENT_EMAIL'] || '',
+      project_id: process.env['FB_PROJECT_ID'] || '',
+      private_key: (process.env['FB_PRIVATE_KEY'] || '').replace(/\\n/g, '\n'),
+      client_email: process.env['FB_CLIENT_EMAIL'] || '',
     },
   };
 }
