a2075bd234b27a22a9f00f9f71396efd5fdae0ae
suraj <surajrace21@gmail.com>
2025-07-20 19:20:57 +0530

    hardcoded for testing

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import { healthRoutes } from './health.routes';
 import { authRoutes } from './auth.routes';
 import configRoutes from './config.routes';
-import { config } from '../config/environment';
+// import { config } from '../config/environment';
 
 const router = Router();
 
